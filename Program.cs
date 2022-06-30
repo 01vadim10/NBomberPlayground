0357dd68c3f80909c456901f9ea5bcab80262608
Vadim Zylevich <vzy@leapwork.com>
2022-06-30 21:18:17 +0200

    NBomber initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using NBomber.Contracts;
+using NBomber.CSharp;
+
+Console.WriteLine("Hello, World!");
+
+var step = Step.Create("step", async context =>
+{
+    await Task.Delay(TimeSpan.FromMilliseconds(500));
+    return Response.Ok();
+});
+
+var scenario = ScenarioBuilder.CreateScenario("hello_world", step);
+
+NBomberRunner
+    .RegisterScenarios(scenario)
+    .Run();
